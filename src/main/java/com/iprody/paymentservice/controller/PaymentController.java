4a08f14b60b00d52981589ee7bbd149b86ffe7bc
tanya <t.a.gordeeva1997@gmail.com>
2024-04-16 21:36:34 +0300

    Payment Service: Payment Domain

--- src/main/java/com/iprody/paymentservice/controller/PaymentController.java
+++ src/main/java/com/iprody/paymentservice/controller/PaymentController.java
@@ -1,7 +1,47 @@
 package com.iprody.paymentservice.controller;
 
-import org.springframework.stereotype.Controller;
+import com.iprody.paymentservice.dto.PaymentDto;
+import com.iprody.paymentservice.entity.Payment;
+import com.iprody.paymentservice.service.PaymentService;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
 
-@Controller
+import java.net.URI;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/payment")
 public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    private final ModelMapper modelMapper;
+
+    @GetMapping()
+    public Mono<ResponseEntity<PaymentDto>> findPaymentById(@RequestParam Long id) {
+        var paymentMono = paymentService.findPaymentById(id);
+        return paymentMono.flatMap(foundedUser -> Mono.just(ResponseEntity.ok(modelMapper.map(foundedUser, PaymentDto.class)))
+                .switchIfEmpty(Mono.just(ResponseEntity.notFound().build())));
+    }
+
+
+    @PostMapping("/add")
+    public Mono<ResponseEntity<PaymentDto>> savePayment(@RequestBody PaymentDto paymentDto) {
+        var createdPayment = paymentService.createPayment(modelMapper.map(paymentDto, Payment.class));
+        return createdPayment.flatMap(value ->
+                        Mono.just(ResponseEntity.created(URI.create("/payments/" + value.getId()))
+                                .body(modelMapper.map(value, PaymentDto.class))))
+                .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
+    }
+
+    @PostMapping("/update")
+    public Mono<ResponseEntity<PaymentDto>> updatePayment(@RequestParam Long id, @RequestBody PaymentDto paymentDto) {
+        var paymentMono = paymentService.updatePayment(id, modelMapper.map(paymentDto, Payment.class));
+        return paymentMono.flatMap(updatedPayment -> Mono.just(ResponseEntity.ok(modelMapper.map(updatedPayment, PaymentDto.class)))
+                .switchIfEmpty(Mono.just(ResponseEntity.notFound().build())));
+    }
+
 }
--- src/main/java/com/iprody/paymentservice/service/PaymentService.java
+++ src/main/java/com/iprody/paymentservice/service/PaymentService.java
@@ -1,7 +1,46 @@
 package com.iprody.paymentservice.service;
 
+import com.iprody.paymentservice.entity.Payment;
+import com.iprody.paymentservice.repository.PaymentRepository;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.util.NoSuchElementException;
 
 @Service
+@AllArgsConstructor
 public class PaymentService {
+
+    private final PaymentRepository paymentRepository;
+
+
+    @Transactional
+    public Mono<Payment> createPayment(Payment payment) {
+        return Mono.just(paymentRepository.save(payment));
+    }
+
+    @Transactional
+    public Mono<Payment> findPaymentById(Long id) {
+        var foundedPayment = paymentRepository.findById(id);
+        return foundedPayment.map(Mono::just).orElseGet(() ->
+                Mono.error(new NoSuchElementException("No payment with id: " + id)));
+    }
+
+    @Transactional
+    public Mono<Payment> updatePayment(Long id, Payment updatedPayment) {
+        var paymentOptional = paymentRepository.findById(id);
+        if (paymentOptional.isPresent()) {
+            var existingPayment = paymentOptional.get();
+            existingPayment.setAmount(updatedPayment.getAmount());
+            existingPayment.setCurrency(updatedPayment.getCurrency());
+            existingPayment.setStatus(updatedPayment.getStatus());
+            existingPayment.setCreatedAt(updatedPayment.getCreatedAt());
+            existingPayment.setUpdatedAt(updatedPayment.getUpdatedAt());
+            return Mono.just(paymentRepository.save(existingPayment));
+        } else {
+            return Mono.error(new NoSuchElementException("No payment with id: " + id));
+        }
+    }
 }
